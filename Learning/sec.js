de7ce3260b59f36379820c36bc0f1dbd3e17f078
Abhishek Jachak <abhishekjachak22@gmail.com>
2023-06-12 02:34:09 +0530

    yeah yippe

---
+++ Learning/sec.js
@@ -0,0 +1,107 @@
+
+///--------------------------------------------
+// //switch statement
+// let day=4;
+// switch(day){
+//     case 1:
+//         console.log("Monday")
+//         break;
+//     case 2:
+//         console.log("Tuesday")
+//         break;
+//     case 3:
+//         console.log("Wednesday")
+//         break;
+//     case 4:
+//         console.log("Thursday") 
+//         break;
+//     case 5:
+//         console.log("Friday")
+//         break;
+//     case 6:
+//         console.log("Saturday")
+//         break;
+//     case 7:
+//         console.log("Sunday")
+//         break;
+//     default:
+//         console.log("Invalid day bro")   
+//         break; 
+// }
+///--------------------------------------------
+
+
+///--------------------------------------------
+//input taking   //nested if-else
+// let n=prompt("Enter n: ")
+// console.log(n);
+// let win=33
+// if(n>win){console.log("Better")}
+// else{
+//     if(n>=25)console.log("Excellent")
+//     else if(n>=28)console.log("Nice")
+//     else console.log("Invest from today then")
+// }
+///--------------------------------------------
+
+
+///--------------------------------------------
+//if-else condition
+// let age=21
+// drink= age>=18 ? "Wine":"Milk";
+// console.log(drink)
+///--------------------------------------------
+
+
+///--------------------------------------------
+//topic-undefined,null,BigInt
+// let krj;
+// console.log(krj);
+// console.log(typeof krj);
+
+// let kj= 13n,ty=12n;
+// console.log(kj+ty);
+// console.log(typeof kj+ty);
+///--------------------------------------------
+
+
+///--------------------------------------------
+//template string
+// let namee="Robin";
+// namee="navin"
+// let age=20;
+// let aboutMe=`my name is ${namee} and age is ${age}`
+// console.log(aboutMe)
+///--------------------------------------------
+
+
+// //----------------------------------------
+// //string concatenation
+// let str1="uk",str2="ra";
+// let newStr=str1+str2;
+// console.log(newStr);
+// let newStr2=+str1 + +str2;
+// console.log(typeof newStr2);
+// //----------------------------------------
+
+
+
+// //----------------------------------------
+// //conversion
+// var namee="Abhishek   "
+// console.log("HELLO WORLD");
+// newNamee=namee.trim();
+// console.log(newNamee.length); //name is a global var+special meaning ,so namee used
+// newNamee=namee.slice(0,3)  ; 
+// age=21
+// age=age + "" ; 
+// // str="love";
+// str = +"study";
+
+// console.log(typeof age);
+// console.log(typeof 21);
+
+// console.log(typeof str);
+// console.log(typeof "study");
+// //----------------------------------------
+    
---
+++ Learning/thirdArr.js
@@ -0,0 +1,79 @@
+//Array Functions
+///-------------------------------------------------------
+let favs=["Methi","Bhakar","Basundi","Rabadi","Panir",297]
+favs.push(45);
+favs.pop();
+favs.shift();       
+favs.unshift("LOVE");
+console.log(favs)
+favs.splice(2, 1, "pavBhaji");
+console.log("After splice(2, 1, 'pavBhaji'): ", favs);
+///-------------------------------------------------------
+
+
+
+/*
+///-------------------------------------------------------
+//Array destructuring
+const fat=["rice","sleep","stress"]
+fat.push("rig")
+// fat=["gbu"]
+let [a1,a2,a3,a4]=fat
+console.log("My Arr1 value is",a1)
+console.log("My Arr4 value is",a4)
+///-------------------------------------------------------
+
+
+///-------------------------------------------------------
+//For loop:
+let favs=["Methi","Bhakar","Basundi","Rabadi","Panir",297]
+console.log(favs)
+// let FAV= `${favs[0]} is my favourite !`
+// console.log(FAV)
+console.log(`${favs[0]} is my favourite !`)
+console.log(favs[favs.length-2])
+console.log("\n")
+for(let i=0;i<favs.length-1;i++){
+    console.log(favs[i])
+    console.log(favs[i].toUpperCase())
+    console.log(favs[i].toLocaleUpperCase())
+}
+///-------------------------------------------------------
+
+
+
+///-------------------------------------------------------
+///Arrays
+let car=["BMW", "benz", "Black"]
+// console.log(car)
+// let car2=car;
+// let car2=car.slice(0);
+// let car2=[].concat(car);
+let car2=[...car];       //spread operator
+
+car.push("Audii");
+console.log(car);
+console.log(car2);
+
+let car3=[...car2,"nano",45];
+console.log(car3);
+
+let car4=car3.slice(0).concat(34);
+console.log(car4)
+///-------------------------------------------------------
+
+
+///-------------------------------------------------------
+// console.log("I Know You")
+// let car=["BMW", "benz", "Black"]
+// console.log(car)
+// console.log(car[1])
+// console.log(typeof car)
+// // console.log(Array.isArray(car))
+// car[3]="Uk"
+// car.push("Audi")
+// console.log(car)
+// console.log(car.pop())
+// console.log(car)
+///-------------------------------------------------------
+*/
