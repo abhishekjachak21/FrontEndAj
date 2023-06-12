53d9621f8d9bff707b7609c647c4d6938abe871f
Abhishek Jachak <abhishekjachak22@gmail.com>
2023-06-12 18:26:44 +0530

    all

--- JavaScript/map.js
+++ JavaScript/map.js
@@ -4,9 +4,14 @@ const arr = [2,5,8,4,6];
 
 console.log(arr);
 
-function double(x){
+function double(x){ // double
  return x * 2;}
 
+function binaryOP(x){
+ return x.toString(10);}   //radix parameter 2 for binary, 10 for decimal as it is
+
 const op = arr.map(double);
+const opBin = arr.map(binaryOP);
 
 console.log(op);
+console.log(opBin);
