f281a980b7860dbf76465caa5fd1cded2099faee
Abhishek Jachak <abhishekjachak22@gmail.com>
2023-09-19 15:34:17 +0530

    newHiteshSirCourse

---
+++ HiteshJava/jsHitsh14.js
@@ -0,0 +1,69 @@
+
+
+///---------------------------------------------------
+///flat
+// const harri = [1,2,3,[4,5],6,[6,7,[8,,9]]];
+// console.log(harri);
+// const newArrBro = harri.flat(Infinity); //take (2) as a depth 
+// console.log(newArrBro);                 //but for safety can take infinity
+///---------------------------------------------------
+
+
+///---------------------------------------------------
+//push vs concat
+// const erray = ['abhi', 'shyam', 'rahul'];
+// console.log(erray);
+// const birray = ['nana', 'kaka', 'mama'];
+// console.log(birray);
+// const karray = erray.concat(birray);
+// console.log(karray);
+
+// // const marray = erray.push(birray);
+// // console.log(marray);
+
+// // erray.push(birray);
+// // console.log(erray);
+
+// /// if 3 array u want to merge
+// const oarry = ["car", "cycle", "bike"];
+
+// const darry = [...erray,...birray,...oarry]
+// console.log(darry);
+// console.log(typeof darry);
+///---------------------------------------------------
+
+
+
+
+
+///---------------------------------------------------
+//-> join() method
+// const fruits = ['apple', 'banana', 'cherry'];
+// console.log(fruits);
+// console.log(typeof fruits);
+
+// const joinedString = fruits.join(', '); // Join with a comma and space
+// console.log('Fruits: ' + joinedString);
+// console.log(typeof joinedString);
+// Output: Fruits: apple, banana, cherry
+///---------------------------------------------------
+
+
+
+
+
+
+///----------------------------------------------------
+//18 Sept 2023
+//slice vs splice
+// const arr = [0,1,2,3,4,5,6];
+// console.log(arr);
+
+
+// console.log(arr.slice(1,3));
+// console.log(arr);
+
+// console.log(arr.splice(1,3));
+// console.log(arr);
+///---------------------------------------------------
+
---
+++ HiteshJava/jsHitsh16.js
@@ -0,0 +1,106 @@
+// singleton
+// Object.create
+
+// object literals
+
+// const mySym = Symbol("key1")
+
+
+// const JsUser = {
+//     name: "Hitesh",
+//     "full name": "Hitesh Choudhary",
+//     // [mySym]: "mykey1",
+//     age: 18,
+//     location: "Jaipur",
+//     email: "hitesh@google.com",
+//     isLoggedIn: false,
+//     lastLoginDays: ["Monday", "Saturday"]
+// }
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])   // JsUser.full name u cant do like this
+// console.log(typeof JsUser.lastLoginDays)
+// console.log(JsUser[mySym])
+
+// JsUser.email = "hitesh@chatgpt.com"
+// // Object.freeze(JsUser)
+// JsUser.email = "hitesh@microsoft.com"
+// // console.log(JsUser);
+
+// JsUser.greeting = function(){
+//     console.log("Hello JS user");
+// }
+// JsUser.greetingTwo = function(){
+//     console.log(`Hello JS user, ${this.name}`);
+// }
+
+// console.log(JsUser.greeting());
+// console.log(JsUser.greetingTwo());
+
+
+//all abt SYmbol brio
+
+// const uniqueKey = Symbol('unique key');
+
+// const myObjj = {
+//   [uniqueKey]: "I am value"
+// };
+
+// console.log(myObjj[uniqueKey]); // Output: "I am value"
+
+
+
+
+
+
+// const keyy = Symbol('HUh');
+
+const myObj = {
+    namee : "Abhishek",
+    study : "yes",
+    tp : false,
+    // [keyy] : "dontknow"
+    // [keyy] : Symbol('HUh')    //dekhna padega iska
+}
+console.log(myObj);
+console.log(typeof myObj.study);
+// console.log(typeof myObj[keyy]);
+
+
+myObj.jojo = function() {
+        console.log("Hi bro, why you do so much of HardWork?");
+};
+    
+myObj.jojoLol = function() {
+    console.log(`Still I as ${ this.namee }, not getting any success, idk why?`);
+};
+
+console.log(myObj.jojo());
+console.log(myObj.jojoLol());
+
+
+
+
+// const myObj = {
+//     namee: "Abhishek",
+//     study: "yes",
+//     tp: false,
+// };
+
+// console.log(myObj);
+// console.log(typeof myObj.study);
+
+// myObj.jojo = function() {
+//     console.log("Hi bro, why you do so much of HardWork?");
+// };
+
+// myObj.jojoLol = function() {
+//     console.log(`Still I, as ${this.namee}, not getting any success, why?`);
+// };
+
+// console.log(myObj.jojo);
+// console.log(myObj.jojoLol);
+
+
+
