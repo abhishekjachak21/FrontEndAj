1deb44204a3f9d8cb5ae17949f95dcb3565c0257
Abhishek Jachak <abhishekjachak22@gmail.com>
2023-11-18 01:52:59 +0530

    web

--- HiteshJS/jsHitsh14.js
+++ HiteshJS/jsHitsh14.js
@@ -2,39 +2,80 @@
 
 ///---------------------------------------------------
 ////flat
-// const harri = [1,2,3,[4,5],6,[6,7,[8,,9]]];
-// console.log(harri);
-// // const newArrBro = harri.flat(Infinity); //take (2) as a depth 
-// const newArrBro = harri.flat(4);  
-// console.log(newArrBro);                 //but for safety can take infinity
+const harri = [1,2,3,[4,5],6,[6,7,[8,9]]];
+console.log(harri);
+// const newArrBro = harri.flat(Infinity); //take (2) as a depth 
+const newArrBro = harri.flat(4);  
+console.log(newArrBro);    //but for safety can take infinity
+
+const kaka = [101,102,103];
+harri.push(kaka);   //pushes arr as a element
+console.log(kaka);
+
+console.log(harri);
+const newHarri = harri.flat(5); // merging ALL in ONE
+console.log(newHarri);
+
+///destructuring
+const [aila,maila,kaila,...ohho] = newHarri;
+console.log(aila);
+console.log(maila);
+console.log(kaila);
+console.log(ohho); //baki sagle
 ///---------------------------------------------------
 
 
 ///---------------------------------------------------
-//// push vs concat
-const erray = ['abhi', 'shyam', 'rahul'];
-console.log(erray);
+///18 Sept 2023
+/// push,pop,shift,unshift,slice,splice
+// const fruits = ['apple', 'banana', 'cherry'];
+// console.log(fruits);
+// fruits.push("Krishna"); //add last
+// console.log(fruits);
+// fruits.pop();  //removes last
+// console.log(fruits);
+// fruits.shift();  //removes front
+// console.log(fruits);
+// fruits.unshift("Krishna");  //add front
+// console.log(fruits);
 
-// const birray = ['nana', 'kaka', 'mama'];
-const birray = new Array('nana','kaka','mamaa');  //new way of creating array
-console.log(birray);
+// const arr = [5,6,7,8,9,10];
+// console.log(arr);
 
-const karray = erray.concat(birray);
-console.log(karray);
+// console.log(arr.slice(1,3)); //just chhilka nikala
+// console.log(arr);
 
-// const marray = erray.push(birray);  //array will store like element
-// console.log(marray);  //marray nothing get affected
+// console.log(arr.splice(1,3)); //adha piece kaat dala
+// console.log(arr);
+///---------------------------------------------------
+
+
+
+///---------------------------------------------------
+// //// push vs concat
+// const erray = ['abhi', 'shyam', 'rahul'];
 // console.log(erray);
 
-// erray.push(birray);
-// console.log(erray);
+// // const birray = ['nana', 'kaka', 'mama'];
+// const birray = new Array('nana','kaka','mamaa');  //new way of creating array
+// console.log(birray);
 
-/// if 3 array u want to merge..aur yhi sabse best tarika
-const oarry = ["car", "cycle", "bike"];
+// const karray = erray.concat(birray);
+// console.log(karray);
 
-const darry = [...erray,...birray,...oarry]
-console.log(darry);
-console.log(typeof darry);
+// // const marray = erray.push(birray);  //array will store like element
+// // console.log(marray);  //marray nothing get affected
+// // console.log(erray);
+
+// // erray.push(birray);
+// // console.log(erray);
+
+// /// if 3 array u want to merge..aur yhi sabse best tarika
+// const oarry = ["car", "cycle", "bike"];
+
+// const darry = [...erray,...birray,...oarry]
+// console.log(darry);
+// console.log(typeof darry);
 ///---------------------------------------------------
 
 
@@ -43,32 +84,19 @@ console.log(typeof darry);
 
 ///---------------------------------------------------
 // //-> join() methoday converted into string
-// console.log('Fruits: ' + joinedString);
+
 // const fruits = ['apple', 'banana', 'cherry'];
 // console.log(fruits);
 // console.log(typeof fruits);
 
 // const joinedString = fruits.join(', '); //arr
+// console.log('Fruits: ' + joinedString);
 // console.log(typeof joinedString);
 // // Output: Fruits: apple, banana, cherry
 ///---------------------------------------------------
+//revising this on 11Nov 2023
 
 
 
 
-
-
-///----------------------------------------------------
-//18 Sept 2023
-//slice vs splice
-// const arr = [5,6,7,8,9,10];
-// console.log(arr);
-
-
-// console.log(arr.slice(1,3)); //just chhilka nikala
-// console.log(arr);
-
-// console.log(arr.splice(1,3)); //adha pura kaat dala
-// console.log(arr);
-///---------------------------------------------------
 
--- HiteshJS/jsHitsh16.js
+++ HiteshJS/jsHitsh16.js
@@ -1,87 +1,102 @@
-// singleton
+// constructor -> singleton
 // Object.create
 
-// object literals
 
-// const mySym = Symbol("key1")
+/* 
+Booleans can be objects (if defined with the new keyword)
+Numbers can be objects (if defined with the new keyword)
+Strings can be objects (if defined with the new keyword)
+Dates are always objects
+Maths are always objects
+Regular expressions are always objects
+Arrays are always objects
+Functions are always objects
+Objects are always objects
+*/
 
 
-// const JsUser = {
-//     name: "Hitesh",
-//     "full name": "Hitesh Choudhary",
-//     // [mySym]: "mykey1",
-//     age: 18,
-//     location: "Jaipur",
-//     email: "hitesh@google.com",
-//     isLoggedIn: false,
-//     lastLoginDays: ["Monday", "Saturday"]
+///-------------------------------------------------------
+/// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    // [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+console.log(JsUser.email)
+console.log(JsUser["email"])
+console.log(JsUser["full name"])   // JsUser.full name u cant do like this
+console.log(typeof JsUser.lastLoginDays)
+console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
+
+/// all abt SYmbol brio
+
+const uniqueKey = Symbol('unique key');
+
+const myObjj = {
+  [uniqueKey]: "I am value"
+};
+
+console.log(myObjj[uniqueKey]); // Output: "I am value"
+///-------------------------------------------------------
+
+
+
+
+///-------------------------------------------------------
+// // const keyy = Symbol('HUh');
+// const myObj = {
+//     namee : "Abhishek",
+//     study : "yes",
+//     tp : false,
+//     // [keyy] : "dontknow"
+//     // [keyy] : Symbol('HUh')    //dekhna padega iska
 // }
-
-// console.log(JsUser.email)
-// console.log(JsUser["email"])
-// console.log(JsUser["full name"])   // JsUser.full name u cant do like this
-// console.log(typeof JsUser.lastLoginDays)
-// console.log(JsUser[mySym])
-
-// JsUser.email = "hitesh@chatgpt.com"
-// // Object.freeze(JsUser)
-// JsUser.email = "hitesh@microsoft.com"
-// // console.log(JsUser);
-
-// JsUser.greeting = function(){
-//     console.log("Hello JS user");
-// }
-// JsUser.greetingTwo = function(){
-//     console.log(`Hello JS user, ${this.name}`);
-// }
-
-// console.log(JsUser.greeting());
-// console.log(JsUser.greetingTwo());
+// console.log(myObj);
+// console.log(typeof myObj.study);
+// // console.log(typeof myObj[keyy]);
 
 
-//all abt SYmbol brio
-
-// const uniqueKey = Symbol('unique key');
-
-// const myObjj = {
-//   [uniqueKey]: "I am value"
+// myObj.jojo = function() {
+//         console.log("Hi bro, why you do so much of HardWork?");
+// };
+    
+// myObj.jojoLol = function() {
+//     console.log(`Still I as ${ this.namee }, not getting any success, idk why?`);
 // };
 
-// console.log(myObjj[uniqueKey]); // Output: "I am value"
+// console.log(myObj.jojo());
+// console.log(myObj.jojoLol());
+///-------------------------------------------------------
 
 
 
-
-
-
-// const keyy = Symbol('HUh');
-
-const myObj = {
-    namee : "Abhishek",
-    study : "yes",
-    tp : false,
-    // [keyy] : "dontknow"
-    // [keyy] : Symbol('HUh')    //dekhna padega iska
-}
-console.log(myObj);
-console.log(typeof myObj.study);
-// console.log(typeof myObj[keyy]);
-
-
-myObj.jojo = function() {
-        console.log("Hi bro, why you do so much of HardWork?");
-};
-    
-myObj.jojoLol = function() {
-    console.log(`Still I as ${ this.namee }, not getting any success, idk why?`);
-};
-
-console.log(myObj.jojo());
-console.log(myObj.jojoLol());
-
-
-
-
+///-------------------------------------------------------
 // const myObj = {
 //     namee: "Abhishek",
 //     study: "yes",
@@ -101,10 +116,26 @@ console.log(myObj.jojoLol());
 
 // console.log(myObj.jojo);
 // console.log(myObj.jojoLol);
+///-------------------------------------------------------
+
+
+
+///------------------------------------------------------
+///Symbol
+let symbol1 = Symbol('mySymbol');
+let symbol2 = Symbol('mySymbol is symbol');
+console.log(symbol1); 
+console.log(symbol2); 
+console.log(symbol1 === symbol2); // Output: false
+console.log(symbol1.description); 
+console.log(symbol2.description); 
+///------------------------------------------------------
 
 
 
 
+
+///-------------------------------------------------------
 // destructuring
 // const person = {
 //     firstName: 'John',
@@ -116,13 +147,15 @@ console.log(myObj.jojoLol());
 //   const { firstName, age } = person;
   
 //   console.log(firstName); // Output: "John"
-//   console.log(age);  // Output: "Doe"
+//   console.log(age);  // Output: "30"
 
 
-const numbers = [1, 2, 3, 4, 5,6,67,7,7,7,7,7,7,65,4,3,,2,,12,3,];
+// const numbers = [1, 2, 3, 4, 5,6,67,7,7,7,7,7,7,65,4,3,2,12,3,23];
 
-const [first, second,... fifth] = numbers;
+// const [first, second,...fifth] = numbers;
 
-console.log(first);  // Output: 1
-console.log(second); // Output: 2
-console.log(fifth);  // Output: 5
+// console.log(first);  // Output: 1
+// console.log(second); // Output: 2
+// console.log(fifth);  // Output: 3,4,...23
+///-------------------------------------------------------
+//revising this on 12Nov 2023(Bhosari)
