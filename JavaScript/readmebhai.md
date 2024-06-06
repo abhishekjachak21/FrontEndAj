a8c00f614082507f6081d3a7e391b73d17ef6063
Abhishek Jachak <abhishekjachak22@gmail.com>
2024-06-06 15:09:02 +0530

    react megaproj vdo and js events

---
+++ JavaScript/readmebhai.md
@@ -0,0 +1,21 @@
+
+# Important Javascript concepts you should know before learning React
+
+⚓ Variables and Data Types in JS
+⚓ Functions
+⚓ Control Flow
+⚓ Arrays and Array Methods
+⚓ Objects
+⚓ ES6+ Features
+⚓ Closures
+⚓ Asynchronous JavaScript
+⚓ Scope and Hoisting
+⚓ DOM Manipulation
+⚓ Event Handling
+⚓ Prototype and Inheritance
+⚓ Error Handling
+⚓ Promises and Fetch API
+⚓ JSON
+⚓ Arrow Functions
+⚓ Event Loop
+⚓ Type Coercion
---
+++ ReactJS/18MegaBlogProj/src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import './App.css';
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from './components';
+import { Outlet } from 'react-router-dom';
+
+function App() {
+  const [loading, setLoading] = useState(true); // Is the toy box sleepy?
+  const dispatch = useDispatch(); // Get the magic wand
+
+  useEffect(() => {
+    authService.getCurrentUser() // Check for the key
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({ userData })); // Wake up the helpers
+      } else {
+        dispatch(logout()); // Let the helpers rest
+      }
+    })
+    .finally(() => setLoading(false)); // The toy box is ready!
+  }, [dispatch]);
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header /> {/* Show the top toy */}
+        <main>
+          <Outlet /> {/* Show the main toys */}
+        </main>
+        <Footer /> {/* Show the bottom toy */}
+      </div>
+    </div>
+  ) : null; // If still sleepy, show nothing yet
+}
+
+export default App;
