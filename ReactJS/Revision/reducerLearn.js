76406148c1481e08c3aa2a625562318637ec4676
Abhishek Jachak <abhishekjachak22@gmail.com>
2024-08-22 01:14:33 +0530

    many chnages did

---
+++ ReactJS/Revision/reducerLearn.js
@@ -0,0 +1,13 @@
+
+import react, {useReducer} from react
+
+const UseReducer = () => {
+
+    const [state, dispatch] = useReducer(reducer, initState);
+
+    return(
+        <>
+          
+        </>
+    )
+}
