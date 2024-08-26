293ea2c89ab5f1a63d703d02c518015a62b9d8e9
Abhishek Jachak <abhishekjachak22@gmail.com>
2024-08-27 02:58:17 +0530

    react reducer learnt

--- ReactJS/Revision/reducerLearn.js
+++ ReactJS/Revision/reducerLearn.js
@@ -1,13 +1,80 @@
 
-import react, {useReducer} from react
 
-const UseReducer = () => {
+const { useReducer } = React;
 
-    const [state, dispatch] = useReducer(reducer, initState);
+        const initState = 0;
 
-    return(
-        <>
+        const reducer = (state, action) => {
+            switch (action.type) {
+                case "INC": 
+                    console.log(state);
+                    return state + 1;
+                case "DEC":  console.log(state);
+                    return state - 1;
+                default:
+                    return state;
+            }
+        };
+
+        const UseReducer = () => {
+            const [state, dispatch] = useReducer(reducer, initState);
+
+            return (
+                <> 
+                    <div>
+                        <p>{state}</p>
+                        <div className="arecha">
+                            <button onClick={() => dispatch({ type: "INC" })}>Increase</button>
+                            <button onClick={() => dispatch({ type: "DEC" })}>Decrease</button>
+                        </div>
+                    </div>
+                </>
+            );
+        };
+
+        // Render the component to the root element
+        ReactDOM.render(<UseReducer />, document.getElementById('root'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import react, {useReducer} from react
+
+// initState = 0;
+
+// const reducer = (state, action) =>{
+//     if(action.type === "INC") 
+//         return state;
+//     return state;
+// }
+
+// const UseReducer = () => {
+
+//     const [state, dispatch] = useReducer(reducer, initState);
+
+//     return(
+//         <> 
+//           <div>
+//             <p>{state}</p>
+//                <div className = "arecha">
+//                     <button onClick={()=> dispatch({type:"INC"})} ></button>
+//                     <button onClick={()=> dispatch({type:"DEC"})} ></button>
+//                </div>
+//           </div>
           
-        </>
-    )
-}
+//         </>
+//     )
+// }
+
+// export default UseReducer;
