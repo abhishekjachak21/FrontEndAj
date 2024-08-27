4b69197a956371d4ea499a6e030a974f633d079b
Abhishek Jachak <abhishekjachak22@gmail.com>
2024-08-27 22:25:49 +0530

    context learning today

---
+++ timePass/contextLearn/src/App.jsx
@@ -0,0 +1,34 @@
+
+import React from "react"
+import ComA from "./ComA"
+
+const App = () => {
+  return < ComA />
+}
+
+export default App
+
+
+
+
+
+
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div className="bg-indigo-300">
+//       <h1 ><strong>I am </strong>The Abhishek Jachak</h1>
+//       <h1 >The Abhishek Jachak</h1>
+//       </div>
+//     </>
+//   )
+// }
+
+// export default App
---
+++ timePass/contextLearn/src/ComC.jsx
@@ -0,0 +1,12 @@
+import { comma } from "postcss/lib/list"
+import React from "react"
+
+const ComA = () => {
+    return (
+        <>
+        <h1>ABhi</h1>
+        </>
+    )
+}
+
+export default ComA
